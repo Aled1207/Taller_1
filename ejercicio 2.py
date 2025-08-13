c6f7927ca082a0302210515050dca22081780bf1
Aled1207 <adariorued@gmail.com>
2025-08-12 20:19:23 -0500

    Add files via upload

---
+++ ejercicio 2.py
@@ -0,0 +1,9 @@
+print("edades")
+nombre1=(input("digite su nombre:"))
+edad1=int(input("digite su edad:"))
+nombre2=(input("digite su nombre:"))
+edad2=int(input("digite su edad:"))
+if (edad1>edad2):
+    print(nombre2, "es menor")
+else:
+    print(nombre1, "es menor")
