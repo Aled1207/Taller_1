ed0751802e210b652ef631be2bf7480410f09509
Aled1207 <adariorued@gmail.com>
2025-08-12 20:42:36 -0500

    Add files via upload

---
+++ ejercicio 3.py
@@ -0,0 +1,5 @@
+print("mediciones termicas")
+grados_celsius=float(input("ingrese temperatura en celsius:"))
+Fahrenhei=(grados_celsius * 9/5) + 32
+Kelvin=grados_celsius + 273.15
+print(" su temperatura en Fahrenhei es", Fahrenhei, " y su temperatura en kelvin es:" , Kelvin)
