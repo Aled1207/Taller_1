7d0de62cee76fa82271f8fe85a1a92f7953b1e1e
Aled1207 <adariorued@gmail.com>
2025-08-19 19:04:07 -0500

    Add files via upload

---
+++ ejercicio4.py
@@ -0,0 +1,8 @@
+print("sistema de análisis matemático")
+valor= int(input("digite su valor respecto a 0: "))
+if valor < 0:
+    print ("su valor es negativo")
+if valor == 0:
+    print ("su valor es neutro")
+if valor > 0:
+    print ("su valor es positivo")
